0fec8e006d5e7f325da474f5f6a1d0ac8d05494b
RaulilloSierra <raulitosierradiaz@gmail.com>
2024-09-26 09:15:55 -0500

    opción reinicio de app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useMemo, useReducer } from "react";
 import Form from "./components/Form.tsx";
 import { activityReducer, initialState } from "./reducers/activityReducer.ts";
 import ActivityList from "./components/ActivityList.tsx";
@@ -10,6 +10,9 @@ function App() {
     localStorage.setItem("activities", JSON.stringify(state.activities));
   }, [state.activities]);
 
+  const canRestartApp = () =>
+    useMemo(() => state.activities.length > 0, [state.activities]);
+
   return (
     <>
       <header className="bg-green-600 py-3">
@@ -17,6 +20,13 @@ function App() {
           <h1 className="text-center font-bold text-5xl text-white uppercase py-3">
             contador de calorías
           </h1>
+          <button
+            className="bg-red-700 hover:bg-red-500 text-white p-2 font-bold uppercase cursor-pointer rounded-lg text-base m-3 disabled:bg-gray-200  disabled:cursor-not-allowed"
+            disabled={!canRestartApp()}
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
+            Reiniciar app
+          </button>
         </div>
       </header>
       <section className="bg-green-500 py-10 px-5">
